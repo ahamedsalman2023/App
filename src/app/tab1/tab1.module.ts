b412623afd9453252e9a7ee465249f23b66bcc64
salman@hasnatech.onmicrosoft.com <salman@hasnatech.onmicrosoft.com>
2024-05-15 11:10:30 +0530

    first commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @NgModule({
   imports: [
@@ -15,6 +16,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+ 
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonicSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
-
+  isModalOpen = false;
+  swiperModules = [IonicSlides];
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 }
